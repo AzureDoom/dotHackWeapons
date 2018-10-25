34f4814e0566e98f5207e1b47a0ad2e3f37dc3dc
AzureZhen <rcraft@cyberconnects2.com>
2018-10-25 08:24:59 -0400

    All but 1 class left

--- src/main/java/mod/azure/rcraft/RcraftMod.java
+++ src/main/java/mod/azure/rcraft/RcraftMod.java
@@ -17,7 +17,7 @@ import mod.azure.rcraft.CommonProxy;
 public class RcraftMod {
 
 	public static final String modid = "rcraft";
-	public static final String version = "0.7";
+	public static final String version = "0.8";
 	
 	@SidedProxy(clientSide = "mod.azure.rcraft.ClientProxy", serverSide = "mod.azure.rcraft.CommonProxy")
     public static CommonProxy proxy;
--- src/main/java/mod/azure/rcraft/Register.java
+++ src/main/java/mod/azure/rcraft/Register.java
@@ -13,7 +13,8 @@ public class Register {
 	public static final Item bladeBrandierItem =  new ItemSword("bladebrandier", 19);
 	public static final Item dualGunnerItem =  new ItemSword("dualgunner", 3);
 	public static final Item dualSwordsItem =  new ItemSword("dualswords", 25);
-	public static final Item edgePunisherItem =  new ItemSword("edgepunisher", 20);
+	public static final Item edgePunisherItem =  new ItemSword("edgepunisher", 19);
+	public static final Item edgePunisher1Item =  new ItemSword("edgepunisher1", 1);
 	public static final Item flickReaperItem =  new ItemSword("flickreaper", 9);
 	public static final Item harvestClericItem =  new ItemMagic("harvestcleric", 1, Tier.MASTER, null);
 	public static final Item harvestCleric1Item =  new ItemMagic("harvestcleric1", 1, Tier.MASTER, null);
@@ -42,25 +43,78 @@ public class Register {
 	public static final Item shadowWarlock13Item =  new ItemMagic("shadowwarlock13", 1, Tier.MASTER, null);
 	public static final Item steamGunnerItem =  new ItemSword("steamgunner", 19);
 	public static final Item tribalGrapplerItem =  new ItemSword("tribalgrappler", 11);
-	//public static final Item bladeMasterItem =  new ItemSword("blademaster", 41);
-	//public static final Item heavyAxemanItem =  new ItemSword("heavyaxeman", 49);
-	//public static final Item heavyBladeItem =  new ItemSword("heavyblade", 46);
-	//public static final Item longArmItem =  new ItemSword("longarm", 46);
-	//public static final Item twinBladeItem =  new ItemSword("twinblade", 122);
-	//public static final Item waveMasterItem =  new ItemMagic("wavemaster", 46);
+	public static final Item bladeMasterItem =  new ItemSword("blademaster", 41);
+	public static final Item heavyAxemanItem =  new ItemSword("heavyaxeman", 45);
+	public static final Item heavyBladeItem =  new ItemSword("heavyblade", 38);
+	public static final Item longArmItem =  new ItemSword("longarm", 42);
+	//public static final Item twinBladeItem =  new ItemSword("twinblade", 82);
+	public static final Item waveMasterItem =  new ItemMagic("wavemaster", 1, Tier.MASTER, null);
+	public static final Item waveMaster1Item =  new ItemMagic("wavemaster1", 1, Tier.MASTER, null);
+	public static final Item waveMaster2Item =  new ItemMagic("wavemaster2", 1, Tier.MASTER, null);
+	public static final Item waveMaster3Item =  new ItemMagic("wavemaster3", 1, Tier.MASTER, null);
+	public static final Item waveMaster4Item =  new ItemMagic("wavemaster4", 1, Tier.MASTER, null);
+	public static final Item waveMaster5Item =  new ItemMagic("wavemaster5", 1, Tier.MASTER, null);
+	public static final Item waveMaster6Item =  new ItemMagic("wavemaster6", 1, Tier.MASTER, null);
+	public static final Item waveMaster7Item =  new ItemMagic("wavemaster7", 1, Tier.MASTER, null);
+	public static final Item waveMaster8Item =  new ItemMagic("wavemaster8", 1, Tier.MASTER, null);
+	public static final Item waveMaster9Item =  new ItemMagic("wavemaster9", 1, Tier.MASTER, null);
+	public static final Item waveMaster10Item =  new ItemMagic("wavemaster10", 1, Tier.MASTER, null);
+	public static final Item waveMaster11Item =  new ItemMagic("wavemaster11", 1, Tier.MASTER, null);
+	public static final Item waveMaster12Item =  new ItemMagic("wavemaster12", 1, Tier.MASTER, null);
+	public static final Item waveMaster13Item =  new ItemMagic("wavemaster13", 1, Tier.MASTER, null);
+	public static final Item waveMaster14Item =  new ItemMagic("wavemaster14", 1, Tier.MASTER, null);
+	public static final Item waveMaster15Item =  new ItemMagic("wavemaster15", 1, Tier.MASTER, null);
+	public static final Item waveMaster16Item =  new ItemMagic("wavemaster16", 1, Tier.MASTER, null);
+	public static final Item waveMaster17Item =  new ItemMagic("wavemaster17", 1, Tier.MASTER, null);
+	public static final Item waveMaster18Item =  new ItemMagic("wavemaster18", 1, Tier.MASTER, null);
+	public static final Item waveMaster19Item =  new ItemMagic("wavemaster19", 1, Tier.MASTER, null);
+	public static final Item waveMaster20Item =  new ItemMagic("wavemaster20", 1, Tier.MASTER, null);
+	public static final Item waveMaster21Item =  new ItemMagic("wavemaster21", 1, Tier.MASTER, null);
+	public static final Item waveMaster22Item =  new ItemMagic("wavemaster22", 1, Tier.MASTER, null);
+	public static final Item waveMaster23Item =  new ItemMagic("wavemaster23", 1, Tier.MASTER, null);
+	public static final Item waveMaster24Item =  new ItemMagic("wavemaster24", 1, Tier.MASTER, null);
+	public static final Item waveMaster25Item =  new ItemMagic("wavemaster25", 1, Tier.MASTER, null);
+	public static final Item waveMaster26Item =  new ItemMagic("wavemaster26", 1, Tier.MASTER, null);
+	public static final Item waveMaster27Item =  new ItemMagic("wavemaster27", 1, Tier.MASTER, null);
+	public static final Item waveMaster28Item =  new ItemMagic("wavemaster28", 1, Tier.MASTER, null);
+	public static final Item waveMaster29Item =  new ItemMagic("wavemaster29", 1, Tier.MASTER, null);
+	public static final Item waveMaster30Item =  new ItemMagic("wavemaster30", 1, Tier.MASTER, null);
+	public static final Item waveMaster31Item =  new ItemMagic("wavemaster31", 1, Tier.MASTER, null);
+	public static final Item waveMaster32Item =  new ItemMagic("wavemaster32", 1, Tier.MASTER, null);
+	public static final Item waveMaster33Item =  new ItemMagic("wavemaster33", 1, Tier.MASTER, null);
+	public static final Item waveMaster34Item =  new ItemMagic("wavemaster34", 1, Tier.MASTER, null);
+	public static final Item waveMaster35Item =  new ItemMagic("wavemaster35", 1, Tier.MASTER, null);
+	public static final Item waveMaster36Item =  new ItemMagic("wavemaster36", 1, Tier.MASTER, null);
+	public static final Item waveMaster37Item =  new ItemMagic("wavemaster37", 1, Tier.MASTER, null);
+	public static final Item waveMaster38Item =  new ItemMagic("wavemaster38", 1, Tier.MASTER, null);
+	public static final Item waveMaster39Item =  new ItemMagic("wavemaster39", 1, Tier.MASTER, null);
+	public static final Item waveMaster40Item =  new ItemMagic("wavemaster40", 1, Tier.MASTER, null);
+	public static final Item waveMaster41Item =  new ItemMagic("wavemaster41", 1, Tier.MASTER, null);
+	public static final Item waveMaster42Item =  new ItemMagic("wavemaster42", 1, Tier.MASTER, null);
 	
 	public static final Item[] itemList = new Item[] {
-			bladeBrandierItem, dualSwordsItem, edgePunisherItem,
+			bladeBrandierItem, dualSwordsItem, edgePunisherItem, edgePunisher1Item,
 			flickReaperItem, macabreDancerItem, steamGunnerItem,
 			harvestClericItem, harvestCleric1Item, harvestCleric2Item, 
-			harvestCleric3Item, harvestCleric4Item, harvestCleric5Item, harvestCleric6Item, 
-			harvestCleric7Item, harvestCleric8Item, 
+			harvestCleric3Item, harvestCleric4Item, harvestCleric5Item, 
+			harvestCleric6Item, harvestCleric7Item, harvestCleric8Item, 
 			shadowWarlockItem, shadowWarlock1Item, shadowWarlock2Item, shadowWarlock3Item, 
 			shadowWarlock4Item, shadowWarlock5Item, shadowWarlock6Item, shadowWarlock7Item, 
 			shadowWarlock8Item, shadowWarlock9Item, shadowWarlock10Item, shadowWarlock11Item, 
 			shadowWarlock12Item, shadowWarlock13Item, 
-			lordPartizanItem, tribalGrapplerItem, dualGunnerItem/*,
-			bladeMasterItem,  heavyAxemanItem, heavyBladeItem, longArmItem, twinBladeItem, waveMasterItem*/
+			lordPartizanItem, tribalGrapplerItem, dualGunnerItem,
+			bladeMasterItem,  heavyAxemanItem, heavyBladeItem, longArmItem, /*twinBladeItem,*/
+			waveMasterItem, waveMaster1Item, waveMaster2Item, waveMaster3Item, 
+			waveMaster4Item, waveMaster5Item, waveMaster6Item, waveMaster7Item, 
+			waveMaster8Item, waveMaster9Item, waveMaster10Item, waveMaster11Item, 
+			waveMaster12Item, waveMaster13Item, waveMaster14Item, waveMaster15Item, 
+			waveMaster16Item, waveMaster17Item, waveMaster18Item, waveMaster19Item, 
+			waveMaster20Item, waveMaster21Item, waveMaster22Item, waveMaster23Item, 
+			waveMaster24Item, waveMaster25Item, waveMaster26Item, waveMaster27Item, 
+			waveMaster28Item, waveMaster29Item, waveMaster30Item, waveMaster31Item, 
+			waveMaster32Item, waveMaster33Item, waveMaster34Item, waveMaster35Item, 
+			waveMaster36Item, waveMaster37Item, waveMaster38Item, waveMaster39Item, 
+			waveMaster40Item, waveMaster41Item, waveMaster42Item
 	};
 
 	public static NonNullList<ItemStack> variantList;
