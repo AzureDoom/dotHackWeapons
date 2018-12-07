0e67ad8b9ef5154f79eebf4858290e83d4cb0b57
AzureZhen <rcraft@cyberconnects2.com>
2018-12-06 21:51:07 -0500

    Changed ItemSword to ItemBaseSword

--- src/main/java/mod/azure/rcraft/items/ItemBaseSword.java
+++ src/main/java/mod/azure/rcraft/items/ItemBaseSword.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Multimap;
 
 import mod.azure.rcraft.IMultiType;
 import mod.azure.rcraft.RcraftMod;
@@ -19,20 +20,23 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.Locale;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 
-public class ItemSword extends net.minecraft.item.ItemSword implements IMultiType {
-
+public class ItemBaseSword extends ItemSword implements IMultiType {
 	private final int maxTypes;
 
-	public ItemSword(String name, int maxTypes) {
-		super(EnumHelper.addToolMaterial(name, 1, 0, 4F, 5, 4));
+	public ItemBaseSword(String name, int maxTypes) {
+		super(EnumHelper.addToolMaterial(name, 1, 0, 4F, (4*2), 4));
 		this.maxTypes = maxTypes;
 		
 		this.setUnlocalizedName(name);
