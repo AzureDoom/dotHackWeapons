a325250584ed855998cccaddc5d55f14785fd3e4
AzureZhen <rcraft@cyberconnects2.com>
2018-12-08 21:38:52 -0500

    Moved EventHandler into ClientProxy and RcraftMod as needed.

--- src/main/java/mod/azure/rcraft/RcraftMod.java
+++ src/main/java/mod/azure/rcraft/RcraftMod.java
@@ -1,9 +1,16 @@
 package mod.azure.rcraft;
 
 import mod.azure.rcraft.proxy.CommonProxy;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -11,8 +18,10 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod(modid = RcraftMod.modid, version = RcraftMod.version, dependencies = "required-after:ebwizardry;required-after:forge@[14.23.5.2768,)")
 public class RcraftMod {
@@ -43,4 +52,21 @@ public class RcraftMod {
     public void postInit(FMLPostInitializationEvent e) {
         proxy.postInit();
     }
+    
+    @Mod.EventBusSubscriber
+	public static class RegistrationHandler {
+    	@SubscribeEvent
+    	public static void registerItems(RegistryEvent.Register<Item> event) {
+    		IForgeRegistry<Item> registry = event.getRegistry();
+    		
+    		for (Item item : Register.itemList) {
+    			registry.register(item);
+    		}
+    		
+    		Register.variantList = NonNullList.create();
+    		for (Item item : Register.itemList) {
+    			item.getSubItems(RcraftMod.tab, Register.variantList);
+    		}
+    	}
+	}
 }
--- src/main/java/mod/azure/rcraft/proxy/ClientProxy.java
+++ src/main/java/mod/azure/rcraft/proxy/ClientProxy.java
@@ -1,10 +1,18 @@
 package mod.azure.rcraft.proxy;
 
+import mod.azure.rcraft.IMultiType;
 import mod.azure.rcraft.RcraftMod;
+import mod.azure.rcraft.Register;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
@@ -24,5 +32,13 @@ public class ClientProxy extends CommonProxy {
 	@EventHandler
 	public void postInit() {
 		
+	}
+	
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event) {
+		for (ItemStack variant : Register.variantList) {
+			Item item = variant.getItem();
+			ModelLoader.setCustomModelResourceLocation(item, variant.getItemDamage(), new ModelResourceLocation(item.getRegistryName(), String.format("type=%d", ((IMultiType) item).getType(variant))));
+		}
 	}
 }
