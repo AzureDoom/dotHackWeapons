f98c0a9ecaed76e00b9e3321b30228b523a6404b
AzureZhen <rcraft@cyberconnects2.com>
2019-05-21 02:14:50 -0400

    1.5.1

--- src/main/java/mod/azure/rcraft/RcraftMod.java
+++ src/main/java/mod/azure/rcraft/RcraftMod.java
@@ -45,7 +45,7 @@ public class RcraftMod {
 
 	public static final String modid = "rcraft";
 	public static final String MODNAME = "dotHack Weapons";
-	public static final String version = "1.5.0";
+	public static final String version = "1.5.1";
 	
 	@SidedProxy(clientSide = "mod.azure.rcraft.proxy.ClientProxy", serverSide = "mod.azure.rcraft.proxy.CommonProxy")
     public static CommonProxy proxy;
