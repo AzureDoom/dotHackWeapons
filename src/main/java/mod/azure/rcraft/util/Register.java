bb46ed3ccd809faf3d2355f768602b1717fcece7
AzureZhen <rcraft@cyberconnects2.com>
2019-05-31 02:19:24 -0400

    Updated items to all start name1

--- src/main/java/mod/azure/rcraft/util/Register.java
+++ src/main/java/mod/azure/rcraft/util/Register.java
@@ -21,224 +21,187 @@ public class Register {
 	static {
 		if(Loader.isModLoaded("ebwizardry")) {
 			List<Item> items = new ArrayList<Item>();
-			items.add(new ItemBaseSword("bladebrandier", 3));
-			for (int i = 2; i <= 19; i ++)
+			for (int i = 1; i <= 19; i ++)
 			{
 				items.add(new ItemBaseSword("bladebrandier" + i, 3 * i));
 			}
-			items.add(new ItemBaseGun("dualgunner"));
 			items.add(new ItemBaseGun("dualgunner1"));
 			items.add(new ItemBaseGun("dualgunner2"));
-			items.add(new ItemBaseSword("dualswords", 2));
-			for (int i = 2; i <= 25; i ++)
+			items.add(new ItemBaseGun("dualgunner3"));
+			for (int i = 1; i <= 25; i ++)
 			{
 				items.add(new ItemBaseSword("dualswords" + i, 2 * i));
 			}
-			items.add(new ItemBaseSword("edgepunisher", 4));
-			for (int i = 2; i <= 20; i ++)
+			for (int i = 1; i <= 20; i ++)
 			{
 				items.add(new ItemBaseSword("edgepunisher" + i, 4 * i));
 			}
-			items.add(new ItemBaseSword("flickreaper", 4));
-			for (int i = 2; i <= 9; i ++)
+			for (int i = 1; i <= 9; i ++)
 			{
 				items.add(new ItemBaseSword("flickreaper" + i, 4 * i));
-			}
-			items.add(new ItemBaseSword("lordpartizan", 4));
-			for (int i = 2; i <= 9; i ++)
-			{
 				items.add(new ItemBaseSword("lordpartizan" + i, 4 * i));
 			}
-			items.add(new ItemBaseGun("steamgunner"));      
-			for (int i = 1; i <= 17; i ++)
+			for (int i = 1; i <= 18; i ++)
 			{
 				items.add(new ItemBaseGun("steamgunner" + i));
-			}       
-			items.add(new ItemBaseSword("tribalgrappler", 3));
-			for (int i = 2; i <= 11; i ++)
+			}
+			for (int i = 1; i <= 11; i ++)
 			{
 				items.add(new ItemBaseSword("tribalgrappler" + i, 3 * i));
 			} 
-			items.add(new ItemBaseSword("blademaster", 3));
-			for (int i = 2; i <= 41; i ++)
+			for (int i = 1; i <= 41; i ++)
 			{
 				items.add(new ItemBaseSword("blademaster" + i, 3 * i));
 			}
-			items.add(new ItemBaseSword("heavyaxeman", 4));
-			for (int i = 2; i <= 45; i ++)
+			for (int i = 1; i <= 45; i ++)
 			{
 				items.add(new ItemBaseSword("heavyaxeman" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("heavyblade", 4));
-			for (int i = 2; i <= 38; i ++)
+			for (int i = 1; i <= 38; i ++)
 			{
 				items.add(new ItemBaseSword("heavyblade" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("longarm", 4));
-			for (int i = 2; i <= 42; i ++)
+			for (int i = 1; i <= 42; i ++)
 			{
 				items.add(new ItemBaseSword("longarm" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("twinblade", 2));
-			for (int i = 2; i <= 79; i ++)
+			for (int i = 1; i <= 79; i ++)
 			{
 				items.add(new ItemBaseSword("twinblade" + i, 2 * i));
 			} 
-			items.add(new ItemBaseMagic("harvestcleric", Tier.BASIC, Element.EARTH));
-			items.add(new ItemBaseMagic("harvestcleric1", Tier.BASIC, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("harvestcleric2", Tier.BASIC, Element.HEALING));
-			items.add(new ItemBaseMagic("harvestcleric3", Tier.BASIC, Element.SORCERY));
-			items.add(new ItemBaseMagic("harvestcleric4", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("harvestcleric5", Tier.APPRENTICE, Element.ICE));
-			items.add(new ItemBaseMagic("harvestcleric6", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("harvestcleric7", Tier.ADVANCED, Element.FIRE));
-			items.add(new ItemBaseMagic("harvestcleric8", Tier.MASTER, Element.FIRE));
-			items.add(new ItemBaseMagic("macabredancer", Tier.BASIC, Element.SORCERY));
-			items.add(new ItemBaseMagic("macabredancer1", Tier.APPRENTICE, Element.EARTH));
-			items.add(new ItemBaseMagic("macabredancer2", Tier.APPRENTICE, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("macabredancer3", Tier.ADVANCED, Element.FIRE));
-			items.add(new ItemBaseMagic("macabredancer4", Tier.MASTER, Element.FIRE));
-			items.add(new ItemBaseMagic("shadowwarlock", Tier.BASIC, Element.MAGIC));
-			items.add(new ItemBaseMagic("shadowwarlock1", Tier.BASIC, Element.SORCERY));
-			items.add(new ItemBaseMagic("shadowwarlock2", Tier.BASIC, Element.EARTH));
-			items.add(new ItemBaseMagic("shadowwarlock3", Tier.BASIC, Element.HEALING));
-			items.add(new ItemBaseMagic("shadowwarlock4", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("shadowwarlock5", Tier.APPRENTICE, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("shadowwarlock6", Tier.APPRENTICE, Element.EARTH));
-			items.add(new ItemBaseMagic("shadowwarlock7", Tier.APPRENTICE, Element.ICE));
-			items.add(new ItemBaseMagic("shadowwarlock8", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("shadowwarlock9", Tier.APPRENTICE, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("shadowwarlock10", Tier.ADVANCED, Element.HEALING));
-			items.add(new ItemBaseMagic("shadowwarlock11", Tier.MASTER, Element.EARTH));
-			items.add(new ItemBaseMagic("shadowwarlock12", Tier.ADVANCED, Element.HEALING));
-			items.add(new ItemBaseMagic("shadowwarlock13", Tier.MASTER, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster", Tier.BASIC, Element.MAGIC));
-			items.add(new ItemBaseMagic("wavemaster1", Tier.BASIC, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster2", Tier.BASIC, Element.ICE));
-			items.add(new ItemBaseMagic("wavemaster3", Tier.BASIC, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster4", Tier.BASIC, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster5", Tier.BASIC, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster6", Tier.BASIC, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("wavemaster7", Tier.BASIC, Element.HEALING));
-			items.add(new ItemBaseMagic("wavemaster8", Tier.BASIC, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster9", Tier.APPRENTICE, Element.ICE));
-			items.add(new ItemBaseMagic("wavemaster10", Tier.APPRENTICE, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster11", Tier.APPRENTICE, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster12", Tier.APPRENTICE, Element.MAGIC));
-			items.add(new ItemBaseMagic("wavemaster13", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster14", Tier.APPRENTICE, Element.MAGIC));
+			items.add(new ItemBaseMagic("harvestcleric1", Tier.BASIC, Element.EARTH));
+			items.add(new ItemBaseMagic("harvestcleric2", Tier.BASIC, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("harvestcleric3", Tier.BASIC, Element.HEALING));
+			items.add(new ItemBaseMagic("harvestcleric4", Tier.BASIC, Element.SORCERY));
+			items.add(new ItemBaseMagic("harvestcleric5", Tier.APPRENTICE, Element.SORCERY));
+			items.add(new ItemBaseMagic("harvestcleric6", Tier.APPRENTICE, Element.ICE));
+			items.add(new ItemBaseMagic("harvestcleric7", Tier.APPRENTICE, Element.SORCERY));
+			items.add(new ItemBaseMagic("harvestcleric8", Tier.ADVANCED, Element.FIRE));
+			items.add(new ItemBaseMagic("harvestcleric9", Tier.MASTER, Element.FIRE));
+			items.add(new ItemBaseMagic("macabredancer1", Tier.BASIC, Element.SORCERY));
+			items.add(new ItemBaseMagic("macabredancer2", Tier.APPRENTICE, Element.EARTH));
+			items.add(new ItemBaseMagic("macabredancer3", Tier.APPRENTICE, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("macabredancer4", Tier.ADVANCED, Element.FIRE));
+			items.add(new ItemBaseMagic("macabredancer5", Tier.MASTER, Element.FIRE));
+			items.add(new ItemBaseMagic("shadowwarlock1", Tier.BASIC, Element.MAGIC));
+			items.add(new ItemBaseMagic("shadowwarlock2", Tier.BASIC, Element.SORCERY));
+			items.add(new ItemBaseMagic("shadowwarlock3", Tier.BASIC, Element.EARTH));
+			items.add(new ItemBaseMagic("shadowwarlock4", Tier.BASIC, Element.HEALING));
+			items.add(new ItemBaseMagic("shadowwarlock5", Tier.APPRENTICE, Element.SORCERY));
+			items.add(new ItemBaseMagic("shadowwarlock6", Tier.APPRENTICE, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("shadowwarlock7", Tier.APPRENTICE, Element.EARTH));
+			items.add(new ItemBaseMagic("shadowwarlock8", Tier.APPRENTICE, Element.ICE));
+			items.add(new ItemBaseMagic("shadowwarlock9", Tier.APPRENTICE, Element.SORCERY));
+			items.add(new ItemBaseMagic("shadowwarlock10", Tier.APPRENTICE, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("shadowwarlock11", Tier.ADVANCED, Element.HEALING));
+			items.add(new ItemBaseMagic("shadowwarlock12", Tier.MASTER, Element.EARTH));
+			items.add(new ItemBaseMagic("shadowwarlock13", Tier.ADVANCED, Element.HEALING));
+			items.add(new ItemBaseMagic("shadowwarlock14", Tier.MASTER, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster1", Tier.BASIC, Element.MAGIC));
+			items.add(new ItemBaseMagic("wavemaster2", Tier.BASIC, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster3", Tier.BASIC, Element.ICE));
+			items.add(new ItemBaseMagic("wavemaster4", Tier.BASIC, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster5", Tier.BASIC, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster6", Tier.BASIC, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster7", Tier.BASIC, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("wavemaster8", Tier.BASIC, Element.HEALING));
+			items.add(new ItemBaseMagic("wavemaster9", Tier.BASIC, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster10", Tier.APPRENTICE, Element.ICE));
+			items.add(new ItemBaseMagic("wavemaster11", Tier.APPRENTICE, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster12", Tier.APPRENTICE, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster13", Tier.APPRENTICE, Element.MAGIC));
+			items.add(new ItemBaseMagic("wavemaster14", Tier.APPRENTICE, Element.SORCERY));
 			items.add(new ItemBaseMagic("wavemaster15", Tier.APPRENTICE, Element.MAGIC));
-			items.add(new ItemBaseMagic("wavemaster16", Tier.APPRENTICE, Element.ICE));
-			items.add(new ItemBaseMagic("wavemaster17", Tier.APPRENTICE, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster18", Tier.APPRENTICE, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster19", Tier.APPRENTICE, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster20", Tier.ADVANCED, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("wavemaster21", Tier.ADVANCED, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster16", Tier.APPRENTICE, Element.MAGIC));
+			items.add(new ItemBaseMagic("wavemaster17", Tier.APPRENTICE, Element.ICE));
+			items.add(new ItemBaseMagic("wavemaster18", Tier.APPRENTICE, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster19", Tier.APPRENTICE, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster20", Tier.APPRENTICE, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster21", Tier.ADVANCED, Element.NECROMANCY));
 			items.add(new ItemBaseMagic("wavemaster22", Tier.ADVANCED, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster23", Tier.ADVANCED, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster24", Tier.ADVANCED, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster25", Tier.ADVANCED, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster26", Tier.ADVANCED, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster27", Tier.ADVANCED, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("wavemaster28", Tier.ADVANCED, Element.HEALING));
-			items.add(new ItemBaseMagic("wavemaster29", Tier.ADVANCED, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster30", Tier.ADVANCED, Element.ICE));
-			items.add(new ItemBaseMagic("wavemaster31", Tier.ADVANCED, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster32", Tier.ADVANCED, Element.HEALING));
-			items.add(new ItemBaseMagic("wavemaster33", Tier.ADVANCED, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster34", Tier.ADVANCED, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster35", Tier.MASTER, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("wavemaster36", Tier.MASTER, Element.EARTH));
-			items.add(new ItemBaseMagic("wavemaster37", Tier.MASTER, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster38", Tier.MASTER, Element.FIRE));
-			items.add(new ItemBaseMagic("wavemaster39", Tier.MASTER, Element.NECROMANCY));
-			items.add(new ItemBaseMagic("wavemaster40", Tier.MASTER, Element.HEALING));
-			items.add(new ItemBaseMagic("wavemaster41", Tier.MASTER, Element.LIGHTNING));
-			items.add(new ItemBaseMagic("wavemaster42", Tier.MASTER, Element.SORCERY));
-			items.add(new ItemBaseMagic("wavemaster43", Tier.MASTER, Element.SORCERY)); 
+			items.add(new ItemBaseMagic("wavemaster23", Tier.ADVANCED, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster24", Tier.ADVANCED, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster25", Tier.ADVANCED, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster26", Tier.ADVANCED, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster27", Tier.ADVANCED, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster28", Tier.ADVANCED, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("wavemaster29", Tier.ADVANCED, Element.HEALING));
+			items.add(new ItemBaseMagic("wavemaster30", Tier.ADVANCED, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster31", Tier.ADVANCED, Element.ICE));
+			items.add(new ItemBaseMagic("wavemaster32", Tier.ADVANCED, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster33", Tier.ADVANCED, Element.HEALING));
+			items.add(new ItemBaseMagic("wavemaster34", Tier.ADVANCED, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster35", Tier.ADVANCED, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster36", Tier.MASTER, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("wavemaster37", Tier.MASTER, Element.EARTH));
+			items.add(new ItemBaseMagic("wavemaster38", Tier.MASTER, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster39", Tier.MASTER, Element.FIRE));
+			items.add(new ItemBaseMagic("wavemaster40", Tier.MASTER, Element.NECROMANCY));
+			items.add(new ItemBaseMagic("wavemaster41", Tier.MASTER, Element.HEALING));
+			items.add(new ItemBaseMagic("wavemaster42", Tier.MASTER, Element.LIGHTNING));
+			items.add(new ItemBaseMagic("wavemaster43", Tier.MASTER, Element.SORCERY));
+			items.add(new ItemBaseMagic("wavemaster44", Tier.MASTER, Element.SORCERY)); 
 			itemList = items.toArray(new Item[items.size()]);
 		} else {
 			List<Item> items = new ArrayList<Item>();
-			items.add(new ItemBaseSword("bladebrandier", 3));
-			for (int i = 2; i <= 19; i ++)
+			for (int i = 1; i <= 19; i ++)
 			{
 				items.add(new ItemBaseSword("bladebrandier" + i, 3 * i));
 			}
 			items.add(new ItemBaseGun("dualgunner"));
 			items.add(new ItemBaseGun("dualgunner1"));
 			items.add(new ItemBaseGun("dualgunner2"));
-			items.add(new ItemBaseSword("dualswords", 2));
-			for (int i = 2; i <= 25; i ++)
+			for (int i = 1; i <= 25; i ++)
 			{
 				items.add(new ItemBaseSword("dualswords" + i, 2 * i));
 			}
-			items.add(new ItemBaseSword("edgepunisher", 4));
-			for (int i = 2; i <= 20; i ++)
+			for (int i = 1; i <= 20; i ++)
 			{
 				items.add(new ItemBaseSword("edgepunisher" + i, 4 * i));
 			}
-			items.add(new ItemBaseSword("flickreaper", 4));
-			for (int i = 2; i <= 9; i ++)
+			for (int i = 1; i <= 9; i ++)
 			{
 				items.add(new ItemBaseSword("flickreaper" + i, 4 * i));
-			}
-			items.add(new ItemBaseSword("lordpartizan", 4));
-			for (int i = 2; i <= 9; i ++)
-			{
 				items.add(new ItemBaseSword("lordpartizan" + i, 4 * i));
-			}
-			items.add(new ItemBaseGun("steamgunner"));      
-			for (int i = 1; i <= 17; i ++)
+				items.add(new ItemBaseGun("harvestcleric" + i));
+			}  
+			for (int i = 1; i <= 18; i ++)
 			{
 				items.add(new ItemBaseGun("steamgunner" + i));
 			}
-			items.add(new ItemBaseSword("tribalgrappler", 3));
-			for (int i = 2; i <= 4; i ++)
+			for (int i = 1; i <= 4; i ++)
 			{
 				items.add(new ItemBaseSword("tribalgrappler" + i, 3 * i));
 			} 
-			items.add(new ItemBaseSword("blademaster", 3));
-			for (int i = 2; i <= 41; i ++)
+			for (int i = 1; i <= 41; i ++)
 			{
 				items.add(new ItemBaseSword("blademaster" + i, 3 * i));
 			} 
-			items.add(new ItemBaseSword("heavyaxeman", 4));
-			for (int i = 2; i <= 45; i ++)
+			for (int i = 1; i <= 45; i ++)
 			{
 				items.add(new ItemBaseSword("heavyaxeman" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("heavyblade", 4));
-			for (int i = 2; i <= 38; i ++)
+			for (int i = 1; i <= 38; i ++)
 			{
 				items.add(new ItemBaseSword("heavyblade" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("longarm", 4));
-			for (int i = 2; i <= 42; i ++)
+			for (int i = 1; i <= 42; i ++)
 			{
 				items.add(new ItemBaseSword("longarm" + i, 4 * i));
 			} 
-			items.add(new ItemBaseSword("twinblade", 2));
-			for (int i = 2; i <= 79; i ++)
+			for (int i = 1; i <= 79; i ++)
 			{
 				items.add(new ItemBaseSword("twinblade" + i, 2 * i));
 			} 
-			items.add(new ItemBaseGun("harvestcleric"));
-			for (int i = 1; i <= 8; i ++)
-			{
-				items.add(new ItemBaseGun("harvestcleric" + i));
-			}
-			items.add(new ItemBaseGun("macabredancer"));
-			for (int i = 1; i <= 4; i ++)
+			for (int i = 1; i <= 5; i ++)
 			{
 				items.add(new ItemBaseGun("macabredancer" + i));
 			}
-			items.add(new ItemBaseGun("shadowwarlock"));
-			for (int i = 1; i <= 13; i ++)
+			for (int i = 1; i <= 14; i ++)
 			{
 				items.add(new ItemBaseGun("shadowwarlock" + i));
 			}
-			items.add(new ItemBaseGun("wavemaster"));
-			for (int i = 1; i <= 43; i ++)
+			for (int i = 1; i <= 44; i ++)
 			{
 				items.add(new ItemBaseGun("wavemaster" + i));
 			}
