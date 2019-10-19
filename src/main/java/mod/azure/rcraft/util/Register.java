3a4c588704616536ab20cd6ccca81a5118d8819a
AzureZhen <rcraft@cyberconnects2.com>
2019-10-19 01:48:51 -0400

    Part 1 of making wands upgradeable.

--- src/main/java/mod/azure/rcraft/util/Register.java
+++ src/main/java/mod/azure/rcraft/util/Register.java
@@ -1,8 +1,12 @@
 package mod.azure.rcraft.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,6 +14,7 @@ import com.google.common.collect.ImmutableList;
 
 import electroblob.wizardry.constants.Element;
 import electroblob.wizardry.constants.Tier;
+import electroblob.wizardry.item.ItemWand;
 import mod.azure.rcraft.items.base.ItemBase;
 import mod.azure.rcraft.items.base.ItemBaseGun;
 import mod.azure.rcraft.items.base.ItemBaseSword;
@@ -20,88 +25,46 @@ import mod.azure.rcraft.items.mineandslash.ItemMSStaff;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
 
 public class Register {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	private static final List<String> NOVICE_EARTH = ImmutableList.of("wavemaster45", "wavemaster47", "harvestcleric2");
+	private static final List<String> EARTH = ImmutableList.of("wavemaster45", "wavemaster47", "harvestcleric2",
+			"wavemaster56", "wavemaster46", "wavemaster48", "harvestcleric5", "macabredancer3", "harvestcleric7");
 
-	private static final List<String> NOVICE_LIGHTNING = ImmutableList.of("wavemaster72", "wavemaster73",
-			"harvestcleric1");
+	private static final List<String> LIGHTNING = ImmutableList.of("wavemaster72", "wavemaster73", "harvestcleric1",
+			"wavemaster20", "wavemaster34", "shadowwarlock10");
 
-	private static final List<String> NOVICE_HEALING = ImmutableList.of("wavemaster49", "wavemaster63", "wavemaster64",
-			"wavemaster30", "wavemaster4", "macabredancer2", "wavemaster5");
+	private static final List<String> HEALING = ImmutableList.of("wavemaster49", "wavemaster63", "wavemaster64",
+			"wavemaster30", "wavemaster4", "macabredancer2", "wavemaster5", "harvestcleric4", "harvestcleric3",
+			"wavemaster33", "shadowwarlock11", "shadowwarlock12", "shadowwarlock13", "shadowwarlock14",
+			"shadowwarlock9", "harvestcleric10");
 
-	private static final List<String> NOVICE_SORCERY = ImmutableList.of("wavemaster26", "wavemaster21", "wavemaster19",
+	private static final List<String> SORCERY = ImmutableList.of("wavemaster26", "wavemaster21", "wavemaster19",
 			"wavemaster16", "wavemaster15", "wavemaster14", "wavemaster13", "wavemaster12", "wavemaster9",
-			"wavemaster8", "wavemaster2", "shadowwarlock3");
+			"wavemaster8", "wavemaster2", "shadowwarlock3", "shadowwarlock4", "shadowwarlock5", "shadowwarlock6",
+			"macabredancer4", "macabredancer5", "shadowwarlock7", "harvestcleric6");
 
-	private static final List<String> NOVICE_ICE = ImmutableList.of("wavemaster27", "wavemaster17", "wavemaster10",
-			"wavemaster6", "wavemaster3");
+	private static final List<String> ICE = ImmutableList.of("wavemaster27", "wavemaster17", "wavemaster10",
+			"wavemaster6", "wavemaster3", "wavemaster58", "wavemaster53", "wavemaster66", "harvestcleric8");
 
-	private static final List<String> NOVICE_FIRE = ImmutableList.of("wavemaster59", "wavemaster70", "wavemaster25",
-			"wavemaster18", "wavemaster11");
+	private static final List<String> FIRE = ImmutableList.of("wavemaster59", "wavemaster70", "wavemaster25",
+			"wavemaster18", "wavemaster11", "wavemaster32", "wavemaster39", "wavemaster67", "harvestcleric9");
 
-	private static final List<String> NOVICE_NECROMANCY = ImmutableList.of("wavemaster60", "wavemaster68",
-			"wavemaster28", "wavemaster7");
+	private static final List<String> NECROMANCY = ImmutableList.of("wavemaster60", "wavemaster68", "wavemaster28",
+			"wavemaster7", "wavemaster40", "wavemaster54", "wavemaster55", "shadowwarlock1", "shadowwarlock8",
+			"macabredancer1");
 
-	private static final List<String> NOVICE_MAGIC = ImmutableList.of("wavemaster42", "wavemaster38", "wavemaster74",
+	private static final List<String> MAGIC = ImmutableList.of("wavemaster42", "wavemaster38", "wavemaster74",
 			"wavemaster43", "wavemaster57", "wavemaster61", "wavemaster62", "wavemaster69", "wavemaster71",
-			"wavemaster24", "wavemaster1", "shadowwarlock2");
-
-	private static final List<String> APPRENTICE_EARTH = ImmutableList.of("wavemaster46", "wavemaster48");
-
-	private static final List<String> APPRENTICE_LIGHTNING = ImmutableList.of("wavemaster20");
-
-	private static final List<String> APPRENTICE_HEALING = ImmutableList.of("harvestcleric4", "harvestcleric3");
-
-	private static final List<String> APPRENTICE_SORCERY = ImmutableList.of("shadowwarlock4", "shadowwarlock5");
-
-	private static final List<String> APPRENTICE_ICE = ImmutableList.of("wavemaster58");
-
-	private static final List<String> APPRENTICE_FIRE = ImmutableList.of("wavemaster32");
-
-	private static final List<String> APPRENTICE_NECROMANCY = ImmutableList.of("wavemaster40", "wavemaster54");
-
-	private static final List<String> APPRENTICE_MAGIC = ImmutableList.of("wavemaster36", "wavemaster37",
-			"wavemaster50", "wavemaster52", "wavemaster65", "wavemaster29", "wavemaster22");
-
-	private static final List<String> ADVANCED_EARTH = ImmutableList.of("harvestcleric5", "macabredancer3");
-
-	private static final List<String> ADVANCED_LIGHTNING = ImmutableList.of("wavemaster34");
-
-	private static final List<String> ADVANCED_HEALING = ImmutableList.of("wavemaster33", "shadowwarlock11",
-			"shadowwarlock12", "shadowwarlock13", "shadowwarlock14");
-
-	private static final List<String> ADVANCED_SORCERY = ImmutableList.of("shadowwarlock6", "macabredancer4",
-			"macabredancer5");
-
-	private static final List<String> ADVANCED_ICE = ImmutableList.of("wavemaster53", "wavemaster66");
-
-	private static final List<String> ADVANCED_FIRE = ImmutableList.of("wavemaster39", "wavemaster67");
-
-	private static final List<String> ADVANCED_NECROMANCY = ImmutableList.of("wavemaster55", "shadowwarlock1");
-
-	private static final List<String> ADVANCED_MAGIC = ImmutableList.of("wavemaster35", "wavemaster23");
-
-	private static final List<String> MASTER_EARTH = ImmutableList.of("harvestcleric7");
-
-	private static final List<String> MASTER_LIGHTNING = ImmutableList.of("shadowwarlock10");
-
-	private static final List<String> MASTER_HEALING = ImmutableList.of("shadowwarlock9", "harvestcleric10");
-
-	private static final List<String> MASTER_SORCERY = ImmutableList.of("shadowwarlock7", "harvestcleric6", "redwand1");
-
-	private static final List<String> MASTER_ICE = ImmutableList.of("harvestcleric8");
-
-	private static final List<String> MASTER_FIRE = ImmutableList.of("harvestcleric9");
-
-	private static final List<String> MASTER_NECROMANCY = ImmutableList.of("shadowwarlock8", "macabredancer1");
-
-	private static final List<String> MASTER_MAGIC = ImmutableList.of("wavemaster44", "wavemaster41", "wavemaster51",
-			"wavemaster31");
+			"wavemaster24", "wavemaster1", "shadowwarlock2", "wavemaster36", "wavemaster37", "wavemaster50",
+			"wavemaster52", "wavemaster65", "wavemaster29", "wavemaster22", "wavemaster35", "wavemaster23",
+			"wavemaster44", "wavemaster41", "wavemaster51", "wavemaster31");
 
 	public static Item[] itemList;
 	public static NonNullList<ItemStack> variantList;
@@ -140,144 +103,98 @@ public class Register {
 			LOGGER.debug("Registered without Baubles");
 		}
 		if (Loader.isModLoaded("ebwizardry")) {
-			for (String a : NOVICE_EARTH) {
-				items.add(new ItemEBWand(a, Tier.NOVICE, Element.EARTH));
+			for (String a : EARTH) {
+				items.add(new ItemEBWand(a + "", Tier.NOVICE, Element.EARTH));
+				;
+				items.add(new ItemEBWand(a + "p", Tier.APPRENTICE, Element.EARTH));
+				;
+				items.add(new ItemEBWand(a + "a", Tier.ADVANCED, Element.EARTH));
+				;
+				items.add(new ItemEBWand(a + "m", Tier.MASTER, Element.EARTH));
+				;
 			}
-			;
-			for (String b : NOVICE_LIGHTNING) {
-				items.add(new ItemEBWand(b, Tier.NOVICE, Element.LIGHTNING));
+			for (String b : LIGHTNING) {
+				items.add(new ItemEBWand(b + "", Tier.NOVICE, Element.LIGHTNING));
+				;
+				items.add(new ItemEBWand(b + "p", Tier.APPRENTICE, Element.LIGHTNING));
+				;
+				items.add(new ItemEBWand(b + "a", Tier.ADVANCED, Element.LIGHTNING));
+				;
+				items.add(new ItemEBWand(b + "m", Tier.MASTER, Element.LIGHTNING));
+				;
 			}
-			;
-			for (String c : NOVICE_HEALING) {
-				items.add(new ItemEBWand(c, Tier.NOVICE, Element.HEALING));
+			for (String c : HEALING) {
+				items.add(new ItemEBWand(c + "", Tier.NOVICE, Element.HEALING));
+				;
+				items.add(new ItemEBWand(c + "p", Tier.APPRENTICE, Element.HEALING));
+				;
+				items.add(new ItemEBWand(c + "a", Tier.ADVANCED, Element.HEALING));
+				;
+				items.add(new ItemEBWand(c + "m", Tier.MASTER, Element.HEALING));
+				;
 			}
-			;
-			for (String d : NOVICE_SORCERY) {
-				items.add(new ItemEBWand(d, Tier.NOVICE, Element.SORCERY));
+			for (String d : SORCERY) {
+				items.add(new ItemEBWand(d + "", Tier.NOVICE, Element.SORCERY));
+				;
+				items.add(new ItemEBWand(d + "p", Tier.APPRENTICE, Element.SORCERY));
+				;
+				items.add(new ItemEBWand(d + "a", Tier.ADVANCED, Element.SORCERY));
+				;
+				items.add(new ItemEBWand(d + "m", Tier.MASTER, Element.SORCERY));
+				;
 			}
-			;
-			for (String e : NOVICE_ICE) {
-				items.add(new ItemEBWand(e, Tier.NOVICE, Element.ICE));
+			for (String e : ICE) {
+				items.add(new ItemEBWand(e + "", Tier.NOVICE, Element.ICE));
+				;
+				items.add(new ItemEBWand(e + "p", Tier.APPRENTICE, Element.ICE));
+				;
+				items.add(new ItemEBWand(e + "a", Tier.ADVANCED, Element.ICE));
+				;
+				items.add(new ItemEBWand(e + "m", Tier.MASTER, Element.ICE));
+				;
 			}
-			;
-			for (String f : NOVICE_FIRE) {
-				items.add(new ItemEBWand(f, Tier.NOVICE, Element.FIRE));
+			for (String f : FIRE) {
+				items.add(new ItemEBWand(f + "", Tier.NOVICE, Element.FIRE));
+				;
+				items.add(new ItemEBWand(f + "p", Tier.APPRENTICE, Element.FIRE));
+				;
+				items.add(new ItemEBWand(f + "a", Tier.ADVANCED, Element.FIRE));
+				;
+				items.add(new ItemEBWand(f + "m", Tier.MASTER, Element.FIRE));
+				;
 			}
-			;
-			for (String g : NOVICE_NECROMANCY) {
-				items.add(new ItemEBWand(g, Tier.NOVICE, Element.NECROMANCY));
+			for (String h : NECROMANCY) {
+				items.add(new ItemEBWand(h + "", Tier.NOVICE, Element.NECROMANCY));
+				;
+				items.add(new ItemEBWand(h + "p", Tier.APPRENTICE, Element.NECROMANCY));
+				;
+				items.add(new ItemEBWand(h + "a", Tier.ADVANCED, Element.NECROMANCY));
+				;
+				items.add(new ItemEBWand(h + "m", Tier.MASTER, Element.NECROMANCY));
+				;
 			}
-			;
-			for (String h : NOVICE_MAGIC) {
-				items.add(new ItemEBWand(h, Tier.NOVICE, Element.MAGIC));
+			for (String i : MAGIC) {
+				items.add(new ItemEBWand(i + "", Tier.NOVICE, Element.MAGIC));
+				;
+				items.add(new ItemEBWand(i + "p", Tier.APPRENTICE, Element.MAGIC));
+				;
+				items.add(new ItemEBWand(i + "a", Tier.ADVANCED, Element.MAGIC));
+				;
+				items.add(new ItemEBWand(i + "m", Tier.MASTER, Element.MAGIC));
+				;
 			}
-			;
-			for (String i : APPRENTICE_EARTH) {
-				items.add(new ItemEBWand(i, Tier.APPRENTICE, Element.EARTH));
-			}
-			;
-			for (String j : APPRENTICE_LIGHTNING) {
-				items.add(new ItemEBWand(j, Tier.APPRENTICE, Element.LIGHTNING));
-			}
-			;
-			for (String k : APPRENTICE_HEALING) {
-				items.add(new ItemEBWand(k, Tier.APPRENTICE, Element.HEALING));
-			}
-			;
-			for (String l : APPRENTICE_SORCERY) {
-				items.add(new ItemEBWand(l, Tier.APPRENTICE, Element.SORCERY));
-			}
-			;
-			for (String m : APPRENTICE_ICE) {
-				items.add(new ItemEBWand(m, Tier.APPRENTICE, Element.ICE));
-			}
-			;
-			for (String n : APPRENTICE_FIRE) {
-				items.add(new ItemEBWand(n, Tier.APPRENTICE, Element.FIRE));
-			}
-			;
-			for (String o : APPRENTICE_NECROMANCY) {
-				items.add(new ItemEBWand(o, Tier.APPRENTICE, Element.NECROMANCY));
-			}
-			;
-			for (String q : APPRENTICE_MAGIC) {
-				items.add(new ItemEBWand(q, Tier.APPRENTICE, Element.MAGIC));
-			}
-			;
-			for (String p : ADVANCED_EARTH) {
-				items.add(new ItemEBWand(p, Tier.ADVANCED, Element.EARTH));
-			}
-			;
-			for (String r : ADVANCED_LIGHTNING) {
-				items.add(new ItemEBWand(r, Tier.ADVANCED, Element.LIGHTNING));
-			}
-			;
-			for (String s : ADVANCED_HEALING) {
-				items.add(new ItemEBWand(s, Tier.ADVANCED, Element.HEALING));
-			}
-			;
-			for (String t : ADVANCED_SORCERY) {
-				items.add(new ItemEBWand(t, Tier.ADVANCED, Element.SORCERY));
-			}
-			;
-			for (String u : ADVANCED_ICE) {
-				items.add(new ItemEBWand(u, Tier.ADVANCED, Element.ICE));
-			}
-			;
-			for (String v : ADVANCED_FIRE) {
-				items.add(new ItemEBWand(v, Tier.ADVANCED, Element.FIRE));
-			}
-			;
-			for (String w : ADVANCED_NECROMANCY) {
-				items.add(new ItemEBWand(w, Tier.ADVANCED, Element.NECROMANCY));
-			}
-			;
-			for (String x : ADVANCED_MAGIC) {
-				items.add(new ItemEBWand(x, Tier.ADVANCED, Element.MAGIC));
-			}
-			;
-			for (String y : MASTER_EARTH) {
-				items.add(new ItemEBWand(y, Tier.MASTER, Element.EARTH));
-			}
-			;
-			for (String z : MASTER_LIGHTNING) {
-				items.add(new ItemEBWand(z, Tier.MASTER, Element.LIGHTNING));
-			}
-			;
-			for (String aa : MASTER_HEALING) {
-				items.add(new ItemEBWand(aa, Tier.MASTER, Element.HEALING));
-			}
-			;
-			for (String ab : MASTER_SORCERY) {
-				items.add(new ItemEBWand(ab, Tier.MASTER, Element.SORCERY));
-			}
-			;
-			for (String ac : MASTER_ICE) {
-				items.add(new ItemEBWand(ac, Tier.MASTER, Element.ICE));
-			}
-			;
-			for (String ad : MASTER_FIRE) {
-				items.add(new ItemEBWand(ad, Tier.MASTER, Element.FIRE));
-			}
-			;
-			for (String ae : MASTER_NECROMANCY) {
-				items.add(new ItemEBWand(ae, Tier.MASTER, Element.NECROMANCY));
-			}
-			;
-			for (String af : MASTER_MAGIC) {
-				items.add(new ItemEBWand(af, Tier.MASTER, Element.MAGIC));
-			}
+			items.add(new ItemEBWand("redwand1", Tier.MASTER, Element.SORCERY));
 			;
 			LOGGER.debug("Registered with EB");
 		} else if (Loader.isModLoaded("mmorpg")) {
-			for (int y = 1; y <= 10; y++)
-				items.add(new ItemMSStaff("harvestcleric" + y));
-			for (int y = 1; y <= 5; y++)
-				items.add(new ItemMSStaff("macabredancer" + y));
-			for (int y = 1; y <= 14; y++)
-				items.add(new ItemMSStaff("shadowwarlock" + y));
-			for (int y = 1; y <= 74; y++)
-				items.add(new ItemMSStaff("wavemaster" + y));
+			for (int ag = 1; ag <= 10; ag++)
+				items.add(new ItemMSStaff("harvestcleric" + ag));
+			for (int ah = 1; ah <= 5; ah++)
+				items.add(new ItemMSStaff("macabredancer" + ah));
+			for (int ai = 1; ai <= 14; ai++)
+				items.add(new ItemMSStaff("shadowwarlock" + ai));
+			for (int aj = 1; aj <= 74; aj++)
+				items.add(new ItemMSStaff("wavemaster" + aj));
 			items.add(new ItemMSStaff("redwand1"));
 			LOGGER.debug("Registered with M&S");
 		} else {
@@ -293,10 +210,10 @@ public class Register {
 			LOGGER.debug("Registered without EB or M&S");
 		}
 		if (Loader.isModLoaded("mmorpg")) {
-			for (int y = 1; y <= 4; y++)
-				items.add(new ItemMSBow("dualgunner" + y));
-			for (int y = 1; y <= 18; y++)
-				items.add(new ItemMSBow("steamgunner" + y));
+			for (int ak = 1; ak <= 4; ak++)
+				items.add(new ItemMSBow("dualgunner" + ak));
+			for (int al = 1; al <= 18; al++)
+				items.add(new ItemMSBow("steamgunner" + al));
 			LOGGER.debug("Registered with M&S");
 		} else {
 			for (int i = 1; i <= 4; i++)
