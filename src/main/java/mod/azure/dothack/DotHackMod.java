c77f24dc8832aee2ff368c4d4ec0545097f924ad
AzureZhen <rcraft@cyberconnects2.com>
2019-12-15 23:18:43 -0500

    Alright Tabs are good now.

--- src/main/java/mod/azure/dothack/DotHackMod.java
+++ src/main/java/mod/azure/dothack/DotHackMod.java
@@ -5,11 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 import mod.azure.dothack.config.ModConfig;
 import mod.azure.dothack.proxy.IProxy;
-import mod.azure.dothack.registry.DotHackTabs;
 import mod.azure.dothack.util.LootHandler;
 import mod.azure.dothack.util.MineSlashHandler;
 import mod.azure.dothack.util.WandMap;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
@@ -18,18 +16,15 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = DotHackMod.MODID, version = DotHackMod.VERSION)
+@Mod(modid = DotHackMod.MODID)
 public class DotHackMod {
 
 	public static final String MODID = "rcraft";
 	public static final String MODNAME = "dotHack Weapons";
-	public static final String VERSION = "2.1.4";
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	@SidedProxy(clientSide = "mod.azure.dothack.proxy.ClientProxy", serverSide = "mod.azure.dothack.proxy.ServerProxy")
 	public static IProxy proxy;
-	
-	public static final CreativeTabs MY_TAB = new DotHackTabs("tabRCraft");
 
 	@Mod.Instance
 	public static DotHackMod instance;
--- src/main/java/mod/azure/dothack/registry/DotHackTabs.java
+++ src/main/java/mod/azure/dothack/registry/DotHackTabs.java
@@ -1,30 +1,55 @@
 package mod.azure.dothack.registry;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-import mod.azure.dothack.DotHackMod;
-import mod.azure.dothack.items.mineandslash.ItemMSStaff;
-import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class DotHackTabs extends CreativeTabs {
-	
-	public DotHackTabs(String name) {
-        super(DotHackMod.MODID + "." + name);
-    }
-	
-	@Override
-	public ItemStack createIcon() {
-		return new ItemStack(DotHackItems.redwand);
-	}
- 
+public class DotHackTabs {
+
+	public static final CreativeTabs SWORDS = (new CreativeTabs("swords") {
+		@SideOnly(Side.CLIENT)
+		public ItemStack createIcon() {
+			return new ItemStack(DotHackItems.blademaster);
+		}
+
+		public boolean hasSearchBar() {
+			return true;
+		}
+	}).setBackgroundImageName("item_search.png");
+
+	public static final CreativeTabs MAGIC = (new CreativeTabs("magic") {
+		@SideOnly(Side.CLIENT)
+		public ItemStack createIcon() {
+			return new ItemStack(DotHackItems.redwand);
+		}
+
+		public boolean hasSearchBar() {
+			return true;
+		}
+	}).setBackgroundImageName("item_search.png");
+
+	public static final CreativeTabs GUNS = (new CreativeTabs("guns") {
+		@SideOnly(Side.CLIENT)
+		public ItemStack createIcon() {
+			return new ItemStack(DotHackItems.dualgunner);
+		}
+
+		public boolean hasSearchBar() {
+			return true;
+		}
+	}).setBackgroundImageName("item_search.png");
+
+	public static final CreativeTabs EXTRA = (new CreativeTabs("extra") {
+		@SideOnly(Side.CLIENT)
+		public ItemStack createIcon() {
+			return new ItemStack(DotHackItems.datadrain);
+		}
+
+		public boolean hasSearchBar() {
+			return true;
+		}
+	}).setBackgroundImageName("item_search.png");
+
 }
