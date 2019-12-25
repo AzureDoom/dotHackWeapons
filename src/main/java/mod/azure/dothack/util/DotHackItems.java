61f6dce2ff4e25b69f1284c7f06a1af56923684e
AzureZhen <rcraft@cyberconnects2.com>
2019-12-25 18:33:09 -0500

    Clean up lang and items/tabs.

--- src/main/java/mod/azure/dothack/util/DotHackItems.java
+++ src/main/java/mod/azure/dothack/util/DotHackItems.java
@@ -15,136 +15,136 @@ import net.minecraftforge.fml.common.Mod;
 public class DotHackItems {
 
 	public static Item bladebrandier;
-	public static Item dualswords;
-	public static Item edgepunisher;
-	public static Item flickreaper;
-	public static Item lordpartizan;
-	public static Item tribalgrappler;
-	public static Item blademaster;
-	public static Item heavyaxeman;
-	public static Item heavyblade;
-	public static Item longarm;
-	public static Item twinblade;
 	public static Item harvestcleric;
-	public static Item macabredancer;
-	public static Item shadowwarlock;
-	public static Item redwand;
 	public static Item dualgunner;
-	public static Item steamgunner;
 	public static Item datadrain;
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class RegistryEvents {
 		@SubscribeEvent
 		public static void registerItems(final RegistryEvent.Register<Item> event) {
-			for (int i = 1; i <= 19; i++)
+			for (int i = 1; i <= 8; i++)
 				event.getRegistry()
-						.register(bladebrandier = new SwordItem(ItemTier.DIAMOND, (3 + i) / 3, -2.4F,
-								new Item.Properties().group(DotHackTabs.BladebrandierItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (3 + i) / 3, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
+										.setRegistryName(location("bladebrandier" + i)));
+			event.getRegistry()
+					.register(bladebrandier = new SwordItem(ItemTier.DIAMOND, 4, -2.4F,
+							new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
+									.setRegistryName(location("bladebrandier9")));
+			for (int i = 10; i <= 19; i++)
+				event.getRegistry()
+						.register(new SwordItem(ItemTier.DIAMOND, (3 + i) / 3, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("bladebrandier" + i)));
 			for (int i = 1; i <= 25; i++)
 				event.getRegistry()
-						.register(dualswords = new SwordItem(ItemTier.DIAMOND, (2 + i) / 5, -2.4F,
-								new Item.Properties().group(DotHackTabs.DualswordsItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (2 + i) / 5, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("dualswords" + i)));
 			for (int i = 1; i <= 20; i++)
 				event.getRegistry()
-						.register(edgepunisher = new SwordItem(ItemTier.DIAMOND, (5 + i) / 2, -2.4F,
-								new Item.Properties().group(DotHackTabs.EdgepunisherItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (5 + i) / 2, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("edgepunisher" + i)));
 			for (int i = 1; i <= 9; i++)
 				event.getRegistry().registerAll(
-						flickreaper = new SwordItem(ItemTier.DIAMOND, (20 + i) / 3, -2.4F,
-								new Item.Properties().group(DotHackTabs.FlickreaperItemGroup))
+						new SwordItem(ItemTier.DIAMOND, (20 + i) / 3, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("flickreaper" + i)),
-						lordpartizan = new SwordItem(ItemTier.DIAMOND, (16 + i) / 2, -2.4F,
-								new Item.Properties().group(DotHackTabs.LordpartizanItemGroup))
+						new SwordItem(ItemTier.DIAMOND, (16 + i) / 2, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("lordpartizan" + i)));
 			for (int i = 1; i <= 11; i++)
 				event.getRegistry()
-						.register(tribalgrappler = new SwordItem(ItemTier.DIAMOND, (20 + i) / 5, -2.4F,
-								new Item.Properties().group(DotHackTabs.TribalgrapplerItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (20 + i) / 5, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("tribalgrappler" + i)));
 			for (int i = 1; i <= 75; i++)
 				event.getRegistry()
-						.register(blademaster = new SwordItem(ItemTier.DIAMOND, (3 + i) / 10, -2.4F,
-								new Item.Properties().group(DotHackTabs.BlademasterItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (3 + i) / 10, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("blademaster" + i)));
 			for (int i = 1; i <= 73; i++)
 				event.getRegistry()
-						.register(heavyaxeman = new SwordItem(ItemTier.DIAMOND, (11 + i) / 7, -2.4F,
-								new Item.Properties().group(DotHackTabs.HeavyaxeItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (11 + i) / 7, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("heavyaxeman" + i)));
 			for (int i = 1; i <= 54; i++)
 				event.getRegistry()
-						.register(heavyblade = new SwordItem(ItemTier.DIAMOND, (81 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.HeavybladeItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (81 + i) / 15, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("heavyblade" + i)));
 			for (int i = 1; i <= 41; i++)
 				event.getRegistry()
-						.register(heavyblade = new SwordItem(ItemTier.DIAMOND, (94 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.HeavybladeItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (94 + i) / 15, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("heavybladev" + i)));
 			for (int i = 1; i <= 71; i++)
 				event.getRegistry()
-						.register(longarm = new SwordItem(ItemTier.DIAMOND, (4 + i) / 9, -2.4F,
-								new Item.Properties().group(DotHackTabs.LongarmItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (4 + i) / 9, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("longarm" + i)));
 			for (int i = 1; i <= 81; i++)
 				event.getRegistry()
-						.register(twinblade = new SwordItem(ItemTier.DIAMOND, (2 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.TwinbladeItemGroup))
+						.register(new SwordItem(ItemTier.DIAMOND, (2 + i) / 15, -2.4F,
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
 										.setRegistryName(location("twinblade" + i)));
 			for (int i = 1; i <= 4; i++)
 				event.getRegistry()
-						.register(dualgunner = new BowItem(new Item.Properties().group(DotHackTabs.DualgunnerItemGroup))
-								.setRegistryName(location("dualgunner" + i)));
+						.register(dualgunner = new BowItem(
+								new Item.Properties().group(DotHackTabs.GunsItemGroup).maxDamage(10000))
+										.setRegistryName(location("dualgunner" + i)));
 			for (int i = 1; i <= 18; i++)
-				event.getRegistry().register(
-						steamgunner = new BowItem(new Item.Properties().group(DotHackTabs.SteamgunnerItemGroup))
+				event.getRegistry()
+						.register(new BowItem(new Item.Properties().group(DotHackTabs.GunsItemGroup).maxDamage(10000))
 								.setRegistryName(location("steamgunner" + i)));
 
 			if (ModList.get().isLoaded("mmorpg")) {
 				for (int i = 1; i <= 10; i++)
 					event.getRegistry()
-							.register(harvestcleric = new StaffItem(new Item.Properties().maxDamage(10000), 0)
-									.setRegistryName(location("harvestcleric" + i)));
+							.register(harvestcleric = new StaffItem(
+									new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
+											.setRegistryName(location("harvestcleric" + i)));
 				for (int i = 1; i <= 5; i++)
-					event.getRegistry()
-							.register(macabredancer = new StaffItem(new Item.Properties().maxDamage(10000), 0)
+					event.getRegistry().register(
+							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
 									.setRegistryName(location("macabredancer" + i)));
 				for (int i = 1; i <= 14; i++)
-					event.getRegistry()
-							.register(shadowwarlock = new StaffItem(new Item.Properties().maxDamage(10000), 0)
+					event.getRegistry().register(
+							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
 									.setRegistryName(location("shadowwarlock" + i)));
 				for (int i = 1; i <= 74; i++)
-					event.getRegistry().register(redwand = new StaffItem(new Item.Properties().maxDamage(10000), 0)
-							.setRegistryName(location("wavemaster" + i)));
-				event.getRegistry().register(redwand = new StaffItem(new Item.Properties().maxDamage(10000), 0)
-						.setRegistryName(location("redwand")));
+					event.getRegistry().register(
+							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
+									.setRegistryName(location("wavemaster" + i)));
+				event.getRegistry().register(
+						new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
+								.setRegistryName(location("redwand")));
 			} else {
 				for (int i = 1; i <= 10; i++)
-					event.getRegistry().register(
-							harvestcleric = new BowItem(new Item.Properties().group(DotHackTabs.HarvestclericItemGroup))
-									.setRegistryName(location("harvestcleric" + i)));
+					event.getRegistry()
+							.register(harvestcleric = new BowItem(
+									new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
+											.setRegistryName(location("harvestcleric" + i)));
 				for (int i = 1; i <= 5; i++)
 					event.getRegistry().register(
-							macabredancer = new BowItem(new Item.Properties().group(DotHackTabs.MacabredancerItemGroup))
+							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
 									.setRegistryName(location("macabredancer" + i)));
 				for (int i = 1; i <= 14; i++)
 					event.getRegistry().register(
-							shadowwarlock = new BowItem(new Item.Properties().group(DotHackTabs.ShadowwarlockItemGroup))
+							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
 									.setRegistryName(location("shadowwarlock" + i)));
 				for (int i = 1; i <= 74; i++)
 					event.getRegistry().register(
-							redwand = new BowItem(new Item.Properties().group(DotHackTabs.WavemasterItemGroup))
+							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
 									.setRegistryName(location("wavemaster" + i)));
 				event.getRegistry()
-						.register(redwand = new BowItem(new Item.Properties().group(DotHackTabs.WavemasterItemGroup))
+						.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
 								.setRegistryName(location("redwand")));
 			}
-			event.getRegistry().register(
-					datadrain = new Item(new Item.Properties().group(DotHackTabs.AccessoriesItemGroup).maxStackSize(1))
+			event.getRegistry().register(datadrain = new Item(
+					new Item.Properties().group(DotHackTabs.AccessoriesItemGroup).maxStackSize(1).maxDamage(10000))
 							.setRegistryName(location("datadrain")));
 		}
 
--- src/main/java/mod/azure/dothack/util/DotHackTabs.java
+++ src/main/java/mod/azure/dothack/util/DotHackTabs.java
@@ -7,125 +7,42 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class DotHackTabs {
 
-	public static final ItemGroup BlademasterItemGroup = (new ItemGroup("blademaster") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.blademaster);
-		}
-	});
-
-	public static final ItemGroup BladebrandierItemGroup = (new ItemGroup("bladebrandier") {
+	public static final ItemGroup SwordItemGroup = (new ItemGroup("sword") {
 		@OnlyIn(Dist.CLIENT)
 		public ItemStack createIcon() {
 			return new ItemStack(DotHackItems.bladebrandier);
 		}
-	});
 
-	public static final ItemGroup DualswordsItemGroup = (new ItemGroup("dualswords") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.dualswords);
+		public boolean hasSearchBar() {
+			return true;
 		}
-	});
 
-	public static final ItemGroup TwinbladeItemGroup = (new ItemGroup("twinblade") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.twinblade);
-		}
-	});
+	}).setBackgroundImageName("item_search.png");;
 
-	public static final ItemGroup EdgepunisherItemGroup = (new ItemGroup("edgepunisher") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.edgepunisher);
-		}
-	});
-
-	public static final ItemGroup FlickreaperItemGroup = (new ItemGroup("flickreaper") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.flickreaper);
-		}
-	});
-
-	public static final ItemGroup LordpartizanItemGroup = (new ItemGroup("lordpartizan") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.lordpartizan);
-		}
-	});
-
-	public static final ItemGroup TribalgrapplerItemGroup = (new ItemGroup("tribalgrappler") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.tribalgrappler);
-		}
-	});
-
-	public static final ItemGroup HeavyaxeItemGroup = (new ItemGroup("heavyaxeman") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.heavyaxeman);
-		}
-	});
-
-	public static final ItemGroup HeavybladeItemGroup = (new ItemGroup("heavyblade") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.heavyblade);
-		}
-	});
-
-	public static final ItemGroup LongarmItemGroup = (new ItemGroup("longarm") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.longarm);
-		}
-	});
-
-	public static final ItemGroup HarvestclericItemGroup = (new ItemGroup("harvestcleric") {
+	public static final ItemGroup MagicItemGroup = (new ItemGroup("magic") {
 		@OnlyIn(Dist.CLIENT)
 		public ItemStack createIcon() {
 			return new ItemStack(DotHackItems.harvestcleric);
 		}
-	});
 
-	public static final ItemGroup MacabredancerItemGroup = (new ItemGroup("macabredancer") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.macabredancer);
+		public boolean hasSearchBar() {
+			return true;
 		}
-	});
 
-	public static final ItemGroup ShadowwarlockItemGroup = (new ItemGroup("shadowwarlock") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.shadowwarlock);
-		}
-	});
+	}).setBackgroundImageName("item_search.png");;
 
-	public static final ItemGroup WavemasterItemGroup = (new ItemGroup("wavemaster") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.redwand);
-		}
-	});
-
-	public static final ItemGroup DualgunnerItemGroup = (new ItemGroup("dualgunner") {
+	public static final ItemGroup GunsItemGroup = (new ItemGroup("guns") {
 		@OnlyIn(Dist.CLIENT)
 		public ItemStack createIcon() {
 			return new ItemStack(DotHackItems.dualgunner);
 		}
-	});
 
-	public static final ItemGroup SteamgunnerItemGroup = (new ItemGroup("steamgunner") {
-		@OnlyIn(Dist.CLIENT)
-		public ItemStack createIcon() {
-			return new ItemStack(DotHackItems.steamgunner);
+		public boolean hasSearchBar() {
+			return true;
 		}
-	});
-	
+
+	}).setBackgroundImageName("item_search.png");;
+
 	public static final ItemGroup AccessoriesItemGroup = (new ItemGroup("accessories") {
 		@OnlyIn(Dist.CLIENT)
 		public ItemStack createIcon() {
