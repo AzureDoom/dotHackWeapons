a5ddb605dbfc2ac203b78b8e96f3f6b0f72ab675
AzureZhen <rcraft@cyberconnects2.com>
2019-12-26 23:04:32 -0500

    v1.0.0 Loot System and Models all done!

--- src/main/java/mod/azure/dothack/util/LootHandler.java
+++ src/main/java/mod/azure/dothack/util/LootHandler.java
@@ -1,27 +1,54 @@
 package mod.azure.dothack.util;
 
-import java.util.Set;
-
-import com.google.common.collect.Sets;
-
 import mod.azure.dothack.DotHackMod;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootEntry;
 import net.minecraft.world.storage.loot.LootPool;
-import net.minecraft.world.storage.loot.LootTables;
 import net.minecraft.world.storage.loot.TableLootEntry;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = DotHackMod.MODID)
 public class LootHandler {
-	private static Set<ResourceLocation> tables = Sets.newHashSet(LootTables.CHESTS_SIMPLE_DUNGEON,
-			LootTables.CHESTS_ABANDONED_MINESHAFT, LootTables.CHESTS_DESERT_PYRAMID, LootTables.CHESTS_JUNGLE_TEMPLE,
-			LootTables.CHESTS_WOODLAND_MANSION);
+	@SubscribeEvent
+	public static void lootLoad(LootTableLoadEvent evt) {
+		String prefix = "minecraft:chests/";
+		String name = evt.getName().toString();
 
-	public static void onLootLoad(LootTableLoadEvent event) {
-		if (tables.contains(event.getName())) {
-			event.getTable()
-					.addPool(LootPool.builder().addEntry(TableLootEntry
-							.builder(new ResourceLocation(DotHackMod.MODID, "chests/inject")).weight(1).quality(0))
-							.name("dothack_chest_injection").build());
+		if (name.startsWith(prefix)) {
+			String file = name.substring(name.indexOf(prefix) + prefix.length());
+			switch (file) {
+			case "abandoned_mineshaft":
+			case "desert_pyramid":
+			case "jungle_temple":
+			case "simple_dungeon":
+			case "spawn_bonus_chest":
+			case "stronghold_corridor":
+			case "village_blacksmith":
+			case "end_city_treasure":
+			case "stronghold_crossing":
+			case "underwater_ruin_small":
+			case "underwater_ruin_big":
+			case "buried_treasure":
+			case "shipwreck_treasure":
+			case "pillager_outpost":
+				evt.getTable().addPool(getInjectPool(file));
+				break;
+			default:
+				break;
+			}
 		}
+	}
+
+	private static LootPool getInjectPool(String entryName) {
+		return LootPool.builder().addEntry(getInjectEntry(entryName, 1)).bonusRolls(0, 1).name("dothack_inject")
+				.build();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static LootEntry.Builder getInjectEntry(String name, int weight) {
+		ResourceLocation table = new ResourceLocation(DotHackMod.MODID, "inject/" + name);
+		return TableLootEntry.builder(table).weight(weight);
 	}
 }
