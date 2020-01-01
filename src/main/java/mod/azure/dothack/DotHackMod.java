9c334651daf99f50714e3661161ce965b0e54156
AzureZhen <rcraft@cyberconnects2.com>
2020-01-01 09:28:53 -0500

    v1.0.2 The return of Curios support!

--- src/main/java/mod/azure/dothack/DotHackMod.java
+++ src/main/java/mod/azure/dothack/DotHackMod.java
@@ -4,7 +4,7 @@ import mod.azure.dothack.config.Config;
 import mod.azure.dothack.util.LootHandler;
 import mod.azure.dothack.util.MMORPGHandler;
 import net.minecraftforge.common.MinecraftForge;
-//import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -13,8 +13,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-//import top.theillusivec4.curios.api.CuriosAPI;
-//import top.theillusivec4.curios.api.imc.CurioIMCMessage;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod("dothack")
 public class DotHackMod {
@@ -39,7 +39,7 @@ public class DotHackMod {
 
 	private void enqueueIMC(final InterModProcessEvent event) {
 		if (ModList.get().isLoaded("curios")) {
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("bracelet"));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("bracelet"));
 		}
 		if (ModList.get().isLoaded("mmorpg") && Config.INSTANCE.USE_COMPATIBILITY_ON_ITEMS.get()) {
 			MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
