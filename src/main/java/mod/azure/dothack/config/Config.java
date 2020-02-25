5969922279f421f560a1785f680a0784ee274e13
AzureZhen <rcraft@cyberconnects2.com>
2020-02-24 19:05:03 -0500

    v1.2.0

--- src/main/java/mod/azure/dothack/config/Config.java
+++ src/main/java/mod/azure/dothack/config/Config.java
@@ -10,6 +10,7 @@ import com.electronwill.nightconfig.core.io.WritingMode;
 import mod.azure.dothack.DotHackMod;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class Config {
 
@@ -26,14 +27,53 @@ public class Config {
 		public final BooleanValue USE_COMPATIBILITY_ON_ITEMS;
 		public final BooleanValue USE_CHESTLOOTSYSTEM;
 		public final BooleanValue USE_MINESLASHLOOTSYSTEM;
+		public ConfigValue<Integer> ACCESSORY_MAXDAMAGE;
+		public ConfigValue<Integer> AXE_MAXDAMAGE;
+		public ConfigValue<Integer> STAFF_MAXDAMAGE;
+		public ConfigValue<Integer> SWORD_MAXDAMAGE;
+		public ConfigValue<Integer> BOW_MAXDAMAGE;
+		public ConfigValue<Integer> LANCE_MAXDAMAGE;
+		public ConfigValue<Integer> BROAD_MAXDAMAGE;
+		public ConfigValue<Integer> HANDWEAPON_MAXDAMAGE;
+		public ConfigValue<Integer> SCYTHE_MAXDAMAGE;
 
 		ServerConfig(ForgeConfigSpec.Builder builder) {
-			builder.comment("Mine and Slash Compatibility").push("Mine and Slash Compatibility");
+			builder.comment("Mine and Slash Compatibility").push("Mine and Slash Compatibility")
+					.translation("dothack.config.use_compatibility_on_items");
 			USE_COMPATIBILITY_ON_ITEMS = builder.define("Enable", true);
-			builder.comment("Chest Loot Setting").push("Chest Loot Setting");
+			builder.comment("Chest Loot Setting").push("Chest Loot Setting")
+					.translation("dothack.config.use_chestlootsystem");
 			USE_CHESTLOOTSYSTEM = builder.define("Enable", true);
-			builder.comment("Mine and Slash Loot Setting").push("Mine and Slash Loot Setting");
+			builder.comment("Mine and Slash Loot Setting").push("Mine and Slash Loot Setting")
+					.translation("dothack.config.use_mineslashlootsystem");
 			USE_MINESLASHLOOTSYSTEM = builder.define("Enable", true);
+			builder.comment("Sets the max duriabily of Accessories").push("Sets the max duriabily of Accessories.")
+					.translation("dothack.config.accessory_maxdamage");
+			ACCESSORY_MAXDAMAGE = builder.define("ACCESSORY_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Axes.").push("Sets the max duriabily of Axes.")
+					.translation("dothack.config.axe_maxdamage");
+			AXE_MAXDAMAGE = builder.define("AXE_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Staffs.").push("Sets the max duriabily of Staffs.")
+					.translation("dothack.config.staff_maxdamage");
+			STAFF_MAXDAMAGE = builder.define("STAFF_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Swords.").push("Sets the max duriabily of Swords.")
+					.translation("dothack.config.sword_maxdamage");
+			SWORD_MAXDAMAGE = builder.define("SWORD_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Bows.").push("Sets the max duriabily of Bows.")
+					.translation("dothack.config.bow_maxdamage");
+			BOW_MAXDAMAGE = builder.define("BOW_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Lances.").push("Sets the max duriabily of Lances.")
+					.translation("dothack.config.lance_maxdamage");
+			LANCE_MAXDAMAGE = builder.define("LANCE_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Staffs.").push("Sets the max duriabily of Broad Swords.")
+					.translation("dothack.config.broad_maxdamage");
+			BROAD_MAXDAMAGE = builder.define("BROAD_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Hand Weapons.").push("Sets the max duriabily of Hand Weapons.")
+					.translation("dothack.config.handweapon_maxdamage");
+			HANDWEAPON_MAXDAMAGE = builder.define("HANDWEAPON_MAXDAMAGE", 1000);
+			builder.comment("Sets the max duriabily of Scythe Weapons.")
+					.push("Sets the max duriabily of Scythe Weapons.").translation("dothack.config.scythe_maxdamage");
+			SCYTHE_MAXDAMAGE = builder.define("SCYTHE_MAXDAMAGE", 1000);
 			builder.pop();
 		}
 	}
--- src/main/java/mod/azure/dothack/util/DotHackItems.java
+++ src/main/java/mod/azure/dothack/util/DotHackItems.java
@@ -1,6 +1,7 @@
 package mod.azure.dothack.util;
 
 import mod.azure.dothack.DotHackMod;
+import mod.azure.dothack.config.Config;
 import mod.azure.dothack.items.StaffItem;
 import mod.azure.dothack.items.WandItem;
 import net.minecraft.item.BowItem;
@@ -27,130 +28,153 @@ public class DotHackItems {
 			for (int i = 1; i <= 19; i++)
 				event.getRegistry()
 						.register(bladebrandier = new SwordItem(ItemTier.DIAMOND, (3 + i) / 3, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("bladebrandier" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
+												.setRegistryName(location("bladebrandier" + i)));
 			for (int i = 1; i <= 24; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (2 + i) / 5, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("dualswords" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
+												.setRegistryName(location("dualswords" + i)));
 			for (int i = 1; i <= 20; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (5 + i) / 2, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("edgepunisher" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.BROAD_MAXDAMAGE.get()))
+												.setRegistryName(location("edgepunisher" + i)));
 			for (int i = 1; i <= 9; i++)
 				event.getRegistry().registerAll(
 						new SwordItem(ItemTier.DIAMOND, (20 + i) / 3, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("flickreaper" + i)),
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.SCYTHE_MAXDAMAGE.get()))
+												.setRegistryName(location("flickreaper" + i)),
 						new SwordItem(ItemTier.DIAMOND, (16 + i) / 2, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("lordpartizan" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.LANCE_MAXDAMAGE.get()))
+												.setRegistryName(location("lordpartizan" + i)));
 			for (int i = 1; i <= 11; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (20 + i) / 5, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("tribalgrappler" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.HANDWEAPON_MAXDAMAGE.get()))
+												.setRegistryName(location("tribalgrappler" + i)));
 			for (int i = 1; i <= 75; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (3 + i) / 10, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("blademaster" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
+												.setRegistryName(location("blademaster" + i)));
 			for (int i = 1; i <= 73; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (11 + i) / 7, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
+								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(Config.SERVER.AXE_MAXDAMAGE.get()))
 										.setRegistryName(location("heavyaxeman" + i)));
 			for (int i = 1; i <= 54; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (81 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("heavyblade" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.BROAD_MAXDAMAGE.get()))
+												.setRegistryName(location("heavyblade" + i)));
 			for (int i = 1; i <= 41; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (94 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("heavybladev" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.BROAD_MAXDAMAGE.get()))
+												.setRegistryName(location("heavybladev" + i)));
 			for (int i = 1; i <= 71; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (4 + i) / 9, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("longarm" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.LANCE_MAXDAMAGE.get()))
+												.setRegistryName(location("longarm" + i)));
 			for (int i = 1; i <= 81; i++)
 				event.getRegistry()
 						.register(new SwordItem(ItemTier.DIAMOND, (2 + i) / 15, -2.4F,
-								new Item.Properties().group(DotHackTabs.SwordItemGroup).maxDamage(10000))
-										.setRegistryName(location("twinblade" + i)));
+								new Item.Properties().group(DotHackTabs.SwordItemGroup)
+										.maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
+												.setRegistryName(location("twinblade" + i)));
 			for (int i = 1; i <= 4; i++)
 				event.getRegistry()
-						.register(dualgunner = new BowItem(
-								new Item.Properties().group(DotHackTabs.GunsItemGroup).maxDamage(10000))
+						.register(dualgunner = new BowItem(new Item.Properties().group(DotHackTabs.GunsItemGroup)
+								.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
 										.setRegistryName(location("dualgunner" + i)));
 			for (int i = 1; i <= 18; i++)
 				event.getRegistry()
-						.register(new BowItem(new Item.Properties().group(DotHackTabs.GunsItemGroup).maxDamage(10000))
-								.setRegistryName(location("steamgunner" + i)));
+						.register(new BowItem(new Item.Properties().group(DotHackTabs.GunsItemGroup)
+								.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+										.setRegistryName(location("steamgunner" + i)));
 
 			if (ModList.get().isLoaded("mmorpg")) {
 				for (int i = 1; i <= 10; i++)
-					event.getRegistry().register(
-							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
-									.setRegistryName(location("harvestcleric" + i)));
+					event.getRegistry()
+							.register(new StaffItem(new Item.Properties().maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get())
+									.group(DotHackTabs.MagicItemGroup), 0)
+											.setRegistryName(location("harvestcleric" + i)));
 				for (int i = 1; i <= 5; i++)
-					event.getRegistry().register(
-							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
-									.setRegistryName(location("macabredancer" + i)));
+					event.getRegistry()
+							.register(new StaffItem(new Item.Properties().maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get())
+									.group(DotHackTabs.MagicItemGroup), 0)
+											.setRegistryName(location("macabredancer" + i)));
 				for (int i = 1; i <= 14; i++)
-					event.getRegistry().register(
-							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
-									.setRegistryName(location("shadowwarlock" + i)));
+					event.getRegistry()
+							.register(new StaffItem(new Item.Properties().maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get())
+									.group(DotHackTabs.MagicItemGroup), 0)
+											.setRegistryName(location("shadowwarlock" + i)));
 				for (int i = 1; i <= 74; i++)
-					event.getRegistry().register(
-							new StaffItem(new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
-									.setRegistryName(location("wavemaster" + i)));
+					event.getRegistry()
+							.register(new StaffItem(new Item.Properties().maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get())
+									.group(DotHackTabs.MagicItemGroup), 0).setRegistryName(location("wavemaster" + i)));
 				event.getRegistry()
-						.register(redwand = new StaffItem(
-								new Item.Properties().maxDamage(10000).group(DotHackTabs.MagicItemGroup), 0)
+						.register(redwand = new StaffItem(new Item.Properties()
+								.maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get()).group(DotHackTabs.MagicItemGroup), 0)
 										.setRegistryName(location("redwand")));
 				// event.getRegistry().register(
 				// new StaffItem(new
-				// Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000), 0)
-				// .setRegistryName(location("skeithscythe")));
-				event.getRegistry().register(
-						new WandItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000), 0)
-								.setRegistryName(location("hermitsstick")));
-			} else {
-				for (int i = 1; i <= 10; i++)
-					event.getRegistry().register(
-							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-									.setRegistryName(location("harvestcleric" + i)));
-				for (int i = 1; i <= 5; i++)
-					event.getRegistry().register(
-							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-									.setRegistryName(location("macabredancer" + i)));
-				for (int i = 1; i <= 14; i++)
-					event.getRegistry().register(
-							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-									.setRegistryName(location("shadowwarlock" + i)));
-				for (int i = 1; i <= 74; i++)
-					event.getRegistry().register(
-							new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-									.setRegistryName(location("wavemaster" + i)));
-				event.getRegistry().register(
-						redwand = new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-								.setRegistryName(location("redwand")));
-				// event.getRegistry()
-				// .register(new BowItem(new
-				// Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
+				// Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get()),
+				// 0)
 				// .setRegistryName(location("skeithscythe")));
 				event.getRegistry()
-						.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(10000))
-								.setRegistryName(location("hermitsstick")));
+						.register(new WandItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+								.maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get()), 0)
+										.setRegistryName(location("hermitsstick")));
+			} else {
+				for (int i = 1; i <= 10; i++)
+					event.getRegistry()
+							.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+									.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+											.setRegistryName(location("harvestcleric" + i)));
+				for (int i = 1; i <= 5; i++)
+					event.getRegistry()
+							.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+									.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+											.setRegistryName(location("macabredancer" + i)));
+				for (int i = 1; i <= 14; i++)
+					event.getRegistry()
+							.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+									.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+											.setRegistryName(location("shadowwarlock" + i)));
+				for (int i = 1; i <= 74; i++)
+					event.getRegistry()
+							.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+									.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+											.setRegistryName(location("wavemaster" + i)));
+				event.getRegistry()
+						.register(redwand = new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+								.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get())).setRegistryName(location("redwand")));
+				// event.getRegistry()
+				// .register(new BowItem(new
+				// Item.Properties().group(DotHackTabs.MagicItemGroup).maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+				// .setRegistryName(location("skeithscythe")));
+				event.getRegistry()
+						.register(new BowItem(new Item.Properties().group(DotHackTabs.MagicItemGroup)
+								.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
+										.setRegistryName(location("hermitsstick")));
 			}
-			event.getRegistry().register(datadrain = new Item(
-					new Item.Properties().group(DotHackTabs.AccessoriesItemGroup).maxStackSize(1).maxDamage(10000))
-							.setRegistryName(location("datadrain")));
+			event.getRegistry()
+					.register(datadrain = new Item(new Item.Properties().group(DotHackTabs.AccessoriesItemGroup)
+							.maxStackSize(1).maxDamage(Config.SERVER.ACCESSORY_MAXDAMAGE.get()))
+									.setRegistryName(location("datadrain")));
 		}
 
 		public static ResourceLocation location(String name) {
