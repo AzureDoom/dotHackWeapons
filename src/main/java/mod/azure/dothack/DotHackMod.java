e27c25b26e6e6d057a9e013122adb661fbf370e2
AzureZhen <rcraft@cyberconnects2.com>
2020-02-27 08:24:28 -0500

    v1.3.0

--- src/main/java/mod/azure/dothack/DotHackMod.java
+++ src/main/java/mod/azure/dothack/DotHackMod.java
@@ -6,14 +6,17 @@ import org.apache.logging.log4j.Logger;
 import mod.azure.dothack.config.Config;
 import mod.azure.dothack.util.LootHandler;
 import mod.azure.dothack.util.MMORPGHandler;
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
@@ -48,8 +51,18 @@ public class DotHackMod {
 		if (ModList.get().isLoaded("curios")) {
 			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("bracelet"));
 		}
-		if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
-			MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
+	}
+	
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class GatherDataSubscriber {
+		@SubscribeEvent
+		public static void gatherData(GatherDataEvent event) {
+			DataGenerator gen = event.getGenerator();
+			if (event.includeServer()) {
+				if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
+					gen.addProvider(new MMORPGHandler().getDataPackCreator(gen));
+				}
+			}
 		}
 	}
 }
--- src/main/java/mod/azure/dothack/util/MMORPGHandler.java
+++ src/main/java/mod/azure/dothack/util/MMORPGHandler.java
@@ -1,7 +1,11 @@
 package mod.azure.dothack.util;
 
-import com.robertx22.mine_and_slash.api.MineAndSlashAPI;
-import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.robertx22.mine_and_slash.config.compatible_items.OldConfigItem;
+import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItem;
+import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItemProvider;
 import com.robertx22.mine_and_slash.database.gearitemslots.curios.Necklace;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Axe;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Bow;
@@ -9,71 +13,321 @@ import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Hammer;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Staff;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Sword;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Wand;
+import com.robertx22.mine_and_slash.onevent.data_gen.providers.SlashDataProvider;
 
 import mod.azure.dothack.config.Config;
+import net.minecraft.data.DataGenerator;
 
 public class MMORPGHandler {
 
+	public static String ID = "compatible_items";
+
 	public MMORPGHandler() {
-		for (int i = 1; i <= 10; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:harvestcleric" + i, new ConfigItem().setType(Staff.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 5; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:macabredancer" + i, new ConfigItem().setType(Staff.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 14; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:shadowwarlock" + i, new ConfigItem().setType(Staff.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 74; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:wavemaster" + i, new ConfigItem().setType(Staff.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		MineAndSlashAPI.addCompatibleItem("dothack:redwand", new ConfigItem().setType(Staff.INSTANCE)
-				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 73; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:heavyaxeman" + i, new ConfigItem().setType(Axe.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 71; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:longarm" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 54; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:heavyblade" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 41; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:heavybladev" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 9; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:lordpartizan" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 9; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:flickreaper" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 20; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:edgepunisher" + i, new ConfigItem().setType(Hammer.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 4; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:dualgunner" + i, new ConfigItem().setType(Bow.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 18; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:steamgunner" + i, new ConfigItem().setType(Bow.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 81; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:twinblade" + i, new ConfigItem().setType(Sword.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 25; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:dualswords" + i, new ConfigItem().setType(Sword.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 75; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:blademaster" + i, new ConfigItem().setType(Sword.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 19; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:bladebrandier" + i, new ConfigItem().setType(Sword.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 11; i++)
-			MineAndSlashAPI.addCompatibleItem("dothack:tribalgrappler" + i, new ConfigItem().setType(Sword.INSTANCE)
-					.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		MineAndSlashAPI.addCompatibleItem("dothack:datadrain", new ConfigItem().setType(Necklace.INSTANCE)
-				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()).setAlwaysUnique());
-		MineAndSlashAPI.addCompatibleItem("dothack:hermitsstick", new ConfigItem().setType(Wand.INSTANCE)
-				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+	}
+
+	@SuppressWarnings("rawtypes")
+	public SlashDataProvider getDataPackCreator(DataGenerator gen) {
+		return new CompatibleItemProvider(gen, getList(), ID);
+	}
+
+	public static List<CompatibleItem> getList() {
+		List<CompatibleItem> items = new ArrayList<>();
+		try {
+			for (int type = 1; type <= 1; type++)
+				switch (type) {
+				case 1:
+					for (int bownumber = 1; bownumber <= 4; bownumber++) {
+						OldConfigItem bowconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
+								.setMinRarity(0).setSalvagable(true).setType(Bow.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id1 = "dothack:dualgunner" + bownumber;
+
+						CompatibleItem bow = bowconfig.convertToNewFormat();
+						bow.guid = id1;
+						bow.item_id = id1;
+
+						items.add(bow);
+					}
+				case 2:
+					for (int bownumber = 1; bownumber <= 18; bownumber++) {
+						OldConfigItem bowconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
+								.setMinRarity(0).setSalvagable(true).setType(Bow.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id2 = "dothack:steamgunner" + bownumber;
+
+						CompatibleItem bow = bowconfig.convertToNewFormat();
+						bow.guid = id2;
+						bow.item_id = id2;
+
+						items.add(bow);
+					}
+				case 3:
+					for (int staffnumber = 1; staffnumber <= 10; staffnumber++) {
+						OldConfigItem staffconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Staff.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id3 = "dothack:harvestcleric" + staffnumber;
+
+						CompatibleItem staff = staffconfig.convertToNewFormat();
+						staff.guid = id3;
+						staff.item_id = id3;
+
+						items.add(staff);
+					}
+				case 4:
+					for (int staffnumber = 1; staffnumber <= 5; staffnumber++) {
+						OldConfigItem staffconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Staff.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id4 = "dothack:macabredancer" + staffnumber;
+
+						CompatibleItem staff = staffconfig.convertToNewFormat();
+						staff.guid = id4;
+						staff.item_id = id4;
+
+						items.add(staff);
+					}
+				case 5:
+					for (int axenumber = 1; axenumber <= 73; axenumber++) {
+						OldConfigItem bowconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
+								.setMinRarity(0).setSalvagable(true).setType(Axe.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id5 = "dothack:heavyaxeman" + axenumber;
+
+						CompatibleItem axe = bowconfig.convertToNewFormat();
+						axe.guid = id5;
+						axe.item_id = id5;
+
+						items.add(axe);
+					}
+				case 6:
+					OldConfigItem necklaceconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+							.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Necklace.INSTANCE)
+							.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+					String id6 = "dothack:datadrain";
+
+					CompatibleItem necklaces = necklaceconfig.convertToNewFormat();
+					necklaces.guid = id6;
+					necklaces.item_id = id6;
+
+					items.add(necklaces);
+				case 7:
+					OldConfigItem wandconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
+							.setMinRarity(0).setSalvagable(true).setType(Wand.INSTANCE)
+							.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+					String id7 = "dothack:hermitsstick";
+
+					CompatibleItem wands = wandconfig.convertToNewFormat();
+					wands.guid = id7;
+					wands.item_id = id7;
+
+					items.add(wands);
+				case 8:
+					for (int hammernumber = 1; hammernumber <= 71; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id8 = "dothack:longarm" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id8;
+						hammer.item_id = id8;
+
+						items.add(hammer);
+					}
+				case 9:
+					for (int hammernumber = 1; hammernumber <= 54; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id9 = "dothack:heavyblade" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id9;
+						hammer.item_id = id9;
+
+						items.add(hammer);
+					}
+				case 10:
+					for (int hammernumber = 1; hammernumber <= 41; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id10 = "dothack:heavybladev" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id10;
+						hammer.item_id = id10;
+
+						items.add(hammer);
+					}
+				case 11:
+					for (int hammernumber = 1; hammernumber <= 9; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id11 = "dothack:lordpartizan" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id11;
+						hammer.item_id = id11;
+
+						items.add(hammer);
+					}
+				case 12:
+					for (int hammernumber = 1; hammernumber <= 9; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id12 = "dothack:flickreaper" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id12;
+						hammer.item_id = id12;
+
+						items.add(hammer);
+					}
+				case 13:
+					for (int hammernumber = 1; hammernumber <= 20; hammernumber++) {
+						OldConfigItem hammerconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Hammer.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id13 = "dothack:edgepunisher" + hammernumber;
+
+						CompatibleItem hammer = hammerconfig.convertToNewFormat();
+						hammer.guid = id13;
+						hammer.item_id = id13;
+
+						items.add(hammer);
+					}
+				case 14:
+					for (int staffnumber = 1; staffnumber <= 14; staffnumber++) {
+						OldConfigItem staffconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Staff.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id14 = "dothack:shadowwarlock" + staffnumber;
+
+						CompatibleItem staff = staffconfig.convertToNewFormat();
+						staff.guid = id14;
+						staff.item_id = id14;
+
+						items.add(staff);
+					}
+				case 15:
+					for (int staffnumber = 1; staffnumber <= 74; staffnumber++) {
+						OldConfigItem staffconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Staff.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id15 = "dothack:wavemaster" + staffnumber;
+
+						CompatibleItem staff = staffconfig.convertToNewFormat();
+						staff.guid = id15;
+						staff.item_id = id15;
+
+						items.add(staff);
+					}
+				case 16:
+					OldConfigItem staffconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
+							.setMinRarity(0).setSalvagable(true).setType(Staff.INSTANCE)
+							.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+					String id16 = "dothack:redwand";
+
+					CompatibleItem staff = staffconfig.convertToNewFormat();
+					staff.guid = id16;
+					staff.item_id = id16;
+
+					items.add(staff);
+				case 17:
+					for (int swordnumber = 1; swordnumber <= 81; swordnumber++) {
+						OldConfigItem swordconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id17 = "dothack:twinblade" + swordnumber;
+
+						CompatibleItem sword = swordconfig.convertToNewFormat();
+						sword.guid = id17;
+						sword.item_id = id17;
+
+						items.add(sword);
+					}
+				case 18:
+					for (int swordnumber = 1; swordnumber <= 25; swordnumber++) {
+						OldConfigItem swordconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id18 = "dothack:dualswords" + swordnumber;
+
+						CompatibleItem sword = swordconfig.convertToNewFormat();
+						sword.guid = id18;
+						sword.item_id = id18;
+
+						items.add(sword);
+					}
+				case 19:
+					for (int swordnumber = 1; swordnumber <= 75; swordnumber++) {
+						OldConfigItem swordconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id19 = "dothack:blademaster" + swordnumber;
+
+						CompatibleItem sword = swordconfig.convertToNewFormat();
+						sword.guid = id19;
+						sword.item_id = id19;
+
+						items.add(sword);
+					}
+				case 20:
+					for (int swordnumber = 1; swordnumber <= 19; swordnumber++) {
+						OldConfigItem swordconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id20 = "dothack:bladebrandier" + swordnumber;
+
+						CompatibleItem sword = swordconfig.convertToNewFormat();
+						sword.guid = id20;
+						sword.item_id = id20;
+
+						items.add(sword);
+					}
+				case 21:
+					for (int swordnumber = 1; swordnumber <= 11; swordnumber++) {
+						OldConfigItem swordconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id21 = "dothack:tribalgrappler" + swordnumber;
+
+						CompatibleItem sword = swordconfig.convertToNewFormat();
+						sword.guid = id21;
+						sword.item_id = id21;
+
+						items.add(sword);
+					}
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return items;
 	}
 }
